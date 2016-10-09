4b2179aae86490dcd148c8886f351a7c58f9fe53
MPereira <mpereira132@hotmail.com>
2016-10-09 14:52:34 +0100

    S#5 - login layout

--- www/libs/quagga/example/file_input.js
+++ www/libs/quagga/example/file_input.js
@@ -178,8 +178,6 @@ $(function() {
             $node,
             canvas = Quagga.canvas.dom.image;
 
-        console.log(code);
-
         $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
         $node.find("img").attr("src", canvas.toDataURL());
         $node.find("h4.code").html(code);
