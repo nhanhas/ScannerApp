82257044c5bbf1028b4ada28e5ebbe611b008f3e
MPereira <mpereira132@hotmail.com>
2016-10-10 23:43:55 +0100

    S#13 - barcode as input

--- src/layout/order/OrderController.js
+++ src/layout/order/OrderController.js
@@ -3,7 +3,9 @@ app
 
         $scope.onAddNewProduct = function (){
             console.log('adding...');
-            $scope.initQuagga();
+            //simmulate the "choose file" button
+            var input = document.querySelector("input[type=file]");
+            input.click();
         };
 
 
@@ -17,33 +19,46 @@ app
          * CODE BAR
          *
          */
-
         $scope.lastResult = undefined;
 
-        $scope.initQuagga = function() {
-            Quagga.init({
-                inputStream : {
-                    name : "Live",
-                    type : "LiveStream",
-                    target: document.querySelector('#yourElement')    // Or '#yourElement' (optional)
-                },
-                decoder : {
-                    readers : ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader"]
-                }
-            }, function(err) {
-                if (err) {
-                    console.log(err);
-                    return
-                }
-                console.log("Initialization finished. Ready to start");
-                Quagga.start();
-            });
+        /**
+         * To check out the Live Stream Option
+         * Go to View2Controller
+         */
+
+        //When file is taken from camera or Src
+        $scope.imageUploaded = function (e){
+            $scope.decodeQuagga(URL.createObjectURL(e.files[0]));
         };
 
+        //run de decoder!
+        $scope.decodeQuagga = function(src) {
+            Quagga.decodeSingle({
+                src: src,
+                numOfWorkers: 0,  // Needs to be 0 when used within node
+                inputStream: {
+                    size: 800  // restrict input-size to be 800px in width (long-side)
+                },
+                decoder: {
+                    readers : ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader", "i2of5_reader"]
+                },
+            }, function(result) {
+                if(result.codeResult) {
+                    console.log("result", result.codeResult.code);
+                } else {
+                    console.log("not detected");
+                }
+            });
 
+
+
+        };
+
+        //Quaggajs processes---
         Quagga.onProcessed(function(result) {
             var drawingCtx = Quagga.canvas.ctx.overlay,
-                drawingCanvas = Quagga.canvas.dom.overlay;
+                drawingCanvas = Quagga.canvas.dom.overlay,
+                area;
 
             if (result) {
                 if (result.boxes) {
@@ -62,25 +77,20 @@ app
                 if (result.codeResult && result.codeResult.code) {
                     Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
                 }
+
             }
         });
 
         Quagga.onDetected(function(result) {
-            var code = result.codeResult.code;
+            var code = result.codeResult.code,
+                $node,
+                canvas = Quagga.canvas.dom.image;
 
-            if ($scope.lastResult !== code) {
-                $scope.lastResult = code;
-                var $node = null, canvas = Quagga.canvas.dom.image;
-
-                console.log(result.codeResult);
-
-                $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
-                $node.find("img").attr("src", canvas.toDataURL());
-                $node.find("h4.code").html(code);
-                $("#result_strip ul.thumbnails").prepend($node);
-            }
+            $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
+            $node.find("img").attr("src", canvas.toDataURL());
+            $node.find("h4.code").html(code);
+            $("#result_strip ul.thumbnails").prepend($node);
         });
-
 
 
     }]);
